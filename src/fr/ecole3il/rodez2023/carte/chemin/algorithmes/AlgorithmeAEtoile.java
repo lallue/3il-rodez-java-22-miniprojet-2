bc972bb74ebc20d749de5ffe8ddfb724d9070936
ALLUE <151721459+lallue@users.noreply.github.com>
2024-02-28 16:25:43 +0100

    Implements alorithmAetoile

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeAEtoile.java
@@ -1,5 +1,68 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
-public class AlgorithmeAEtoile {
+import fr.ecole3il.rodez2023.carte.chemin.elements.Graphe;
+import fr.ecole3il.rodez2023.carte.chemin.elements.Noeud;
 
+import java.util.*;
+
+public class AlgorithmeAEtoile<E> implements AlgorithmeChemin<E> {
+
+    protected double heuristique(Noeud<E> n, Noeud<E> cible) {
+        return 0;
+    }
+
+    @Override
+    public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> debut, Noeud<E> cible) {
+        Map<Noeud<E>, Double> coutActuel = new HashMap<>();
+        Map<Noeud<E>, Double> coutTotalEstime = new HashMap<>();
+        Map<Noeud<E>, Noeud<E>> precedent = new HashMap<>();
+        Set<Noeud<E>> explore = new HashSet<>();
+        PriorityQueue<Noeud<E>> filePrioritaire = new PriorityQueue<>(Comparator.comparingDouble(coutTotalEstime::get));
+
+        for (Noeud<E> noeud : graphe.getNoeuds()) {
+            coutTotalEstime.put(noeud, Double.POSITIVE_INFINITY);
+            coutActuel.put(noeud, Double.POSITIVE_INFINITY);
+            precedent.put(noeud, null);
+        }
+        coutActuel.put(debut, 0.0);
+        coutTotalEstime.put(debut, heuristique(debut, cible));
+        filePrioritaire.add(debut);
+
+        while (!filePrioritaire.isEmpty()) {
+            Noeud<E> courant = filePrioritaire.poll();
+            if (courant.equals(cible))
+                break;
+            explore.add(courant);
+
+            for (Noeud<E> voisin : graphe.getVoisins(courant)) {
+                if (explore.contains(voisin))
+                    continue;
+
+                double nouveauCout = coutActuel.get(courant) + graphe.getCoutArete(courant, voisin);
+                if (nouveauCout < coutActuel.get(voisin)) {
+                    precedent.put(voisin, courant);
+                    coutActuel.put(voisin, nouveauCout);
+                    coutTotalEstime.put(voisin, coutActuel.get(voisin) + heuristique(voisin, cible));
+                    if (!filePrioritaire.contains(voisin))
+                        filePrioritaire.add(voisin);
+                }
+            }
+        }
+
+        LinkedList<Noeud<E>> chemin = reconstruireChemin(precedent, cible);
+        Collections.reverse(chemin);
+
+        return new ArrayList<>(chemin);
+    }
+
+    private LinkedList<Noeud<E>> reconstruireChemin(Map<Noeud<E>, Noeud<E>> predecesseur, Noeud<E> cible) {
+        LinkedList<Noeud<E>> chemin = new LinkedList<>();
+        Noeud<E> courant = cible;
+        while (courant != null) {
+            chemin.addFirst(courant);
+            courant = predecesseur.get(courant);
+        }
+        return chemin;
+    }
 }
+
